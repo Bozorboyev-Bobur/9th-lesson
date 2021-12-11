52d3a6ce698145c7861bc00726006a33dfa232eb
Bobur Bazarbayev <bozorboyevbobur2004@gmail.com>
2021-12-11 15:37:38 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,75 @@
+# for name in ['Elbek', 'Zakir']:
+#     pass
+
+# [] - list (mutable)
+# () - tuple (immutable)
+# name = 'Elbek          '
+# print(name.upper())
+# name = name.Lower()
+# name = name.index('s')
+# name = name[3].isalpha()
+# name = name[1].isdigit()
+# name = name.count('12')
+# name = name.find('J')
+# name = name.rstrip()
+# name = name.lstrip()
+
+
+# print(name.index('e', 1, 2))
+# del name
+# print(name)
+
+# name[0] = 'A'
+# ELBEK
+
+
+# str =''
+# for i, letter in enumerate('My name is Elbek. I am 22 years old.I am from Tashkent' ,2):
+#     if i % 2 == 0:str += letter.upper()
+#     else: str += letter
+# print(str)
+
+# str = 'Hello'
+# str2 = ''
+
+# print(str[::-1])
+
+# for ltr in str:
+#     str2 = ltr + str2
+# print(str2)
+
+# str = 'Hello'
+# str2 = ''
+# for i in range(len(str)-1, -1, -1):
+#     str2 += str[i]
+
+# print(str2)
+
+# nameList = list('Elbek')
+# nameList.reverse()
+# str = ''.join(nameList)
+# print(str)
+
+# nums = [1,2,3]
+# t = tuple(nums)
+# print(t)
+
+# print([elem for elem in range(10) if elem % 2 == 0])
+
+# from random import random
+# from random import randint
+
+# print(int(random() * 256))
+# print(randint(0, 255))
+
+# print(round(100 + random() * (200 - 100)))
+
+# from datetime import datetime
+# print(datetime.now())
+
+# from random import random as r, randit as ri
+
+# from random import random as d
+
+# print(d.now())
+
